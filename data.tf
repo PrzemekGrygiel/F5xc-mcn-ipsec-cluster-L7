7fa25da207279f71561bf095f1a4bfe8618709b0
Przemyslaw Grygiel <przemyslaw.grygiel@gmail.com>
2023-03-10 16:35:27 +0100

    origin pool members new IPs

--- data.tf
+++ data.tf
@@ -1,5 +1,6 @@
 data "aws_lb" "internet-vip" {
   #name = "ves-io-vip-6880"
+  depends_on = [volterra_http_loadbalancer.ptf-http-lb,volterra_http_loadbalancer.ptf-https-lb]
   tags = {
     "ves-io/nlb-type"  = "internet-vip",
     "ves-io-site-name" = format("%s-1", var.projectPrefix)
